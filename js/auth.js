b6dd868e11423ff778a80bd4ea24e3ad1230d5b0
Grizzly <makintoch2104@gmail.com>
2022-05-18 17:27:01 +0300

    finish login and register

--- js/auth.js
+++ js/auth.js
@@ -15,52 +15,36 @@ async function getAllUsers() {
     }
 };
 
-//getAllUsers().then(res => console.log(res));
-
-
-/* Get values form register form */ 
-document.forms['registerForm'].addEventListener('submit', e => {
-    e.preventDefault();
-    const form = document.forms['registerForm'];
-    const name = form.elements['name'].value;
-    const email = form.elements['email'].value;
-    const password = form.elements['password'].value;
-    const repeatPassword = form.elements['repeatPassword'].value;
-
-    if(password === repeatPassword){
-        const body = {
-            name: name,
-            email: email,
-            password: password
-        }
-        registerUser(body);
-
-        form.reset();
-    }else{
-        alert('Passwords is not matched');
-    }
-});
-
-
-/* User registration */
-async function registerUser(body) {
-    const response = await fetch(apiURL + '/users', {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            name: body.name,
-            email: body.email,
-            password: body.password
-        })
-    }).then(res => res.json())
-        .then(json => {
-            alert('User registered: ', json.name);
-        })
-        .catch(err => alert("Something problem", err))
-    
-}
-
 
 /*  User login servise  */ 
+
+document.forms['loginForm'].addEventListener('submit', e => {
+    e.preventDefault();
+    const form = document.forms['loginForm'];
+    const email = form.elements['email'].value;
+    const password = form.elements['password'].value;
+
+    const body = {
+        email: email,
+        password: password
+    }
+
+    login(body);
+    form.reset()
+});
+
+async function login(body){
+    const allUsers = await getAllUsers()
+    
+    for(let i = 0; i < allUsers.length; i++){
+        if(body.email == allUsers[i].email){
+            if(body.password == allUsers[i].password){
+                localStorage.setItem('token', 'Bearer:' + allUsers[i].name + ':' + allUsers[i].name.toUpperCase());
+                window.location.href = '/html/top-stories.html';
+                return alert("Succes")
+            }
+            alert('Wrong password');
+        }
+    }
+    alert("Wrong email")
+}
